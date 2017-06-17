029e788097a50bfa564459b957194ed97b9e0de0
Ro_OLeary <roleary81@gmail.com>
2017-06-17 16:28:09 +0100

    Commit salient and theme assets

--- wordpress/wp-content/themes/uplift/page.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-	
-<div class="container">
-
-	<?php sf_base_layout('page'); ?>
-	
-</div>
-
-<?php get_footer(); ?>
--- wordpress/wp-content/themes/uplift/single-galleries.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-
-<div class="container">
-
-	<?php sf_base_layout('single-gallery'); ?>
-	
-</div>
-	
-<?php get_footer(); ?>
